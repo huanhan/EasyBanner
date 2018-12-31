fb60b1bd59ee1527182f81e5275788cc6c9dcf53
豢涵 <huanhanfu@126.com>
2018-12-31 18:53:38 +0800

    修改指示器绘制位置以中心点为坐标

--- easybanner/src/main/java/xin/lrvik/easybanner/indicator/DotIndicator.java
+++ easybanner/src/main/java/xin/lrvik/easybanner/indicator/DotIndicator.java
@@ -1,6 +1,7 @@
 package xin.lrvik.easybanner.indicator;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,20 +10,33 @@ import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
+
+import xin.lrvik.easybanner.R;
 
 /**
  * Author by 豢涵, Email huanhanfu@126.com, Date on 2018/12/31.
  */
-public class DotIndicator extends BaseIndicator {
+public class DotIndicator extends View implements BaseIndicator {
 
     private Paint mPaint;
     private Paint mAniPaint;
-    private Path path;
-    private Path path2;
-    private int size = 4;
+    private Path indicatorPath;
+    private Path selIndicatorpath;
+
+    private int size = 0;
     private int indicatorHeight = 10;
     private int indicatorWidth = 40;
-    private int indicatorMargin = 20;
+    private int indicatorMargin = 50;
+
+    private int selIndicatorHeight = 60;
+    private int selIndicatorWidth = 100;
+
+    private int defColor = Color.parseColor("#DDDDDD");
+    private int selColor = Color.parseColor("#7785D0F7");
+    private boolean defIsFull = true;
+    private boolean selIsFull = true;
+
     private int cPosition;
     private float cPositionOffset;
 
@@ -36,14 +50,38 @@ public class DotIndicator extends BaseIndicator {
 
     public DotIndicator(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        path = new Path();
-        path2 = new Path();
+        indicatorPath = new Path();
+        selIndicatorpath = new Path();
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mAniPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaint.setColor(Color.parseColor("#DDDDDD"));
-        mAniPaint.setColor(Color.parseColor("#7785D0F7"));
+        mPaint.setColor(defColor);
+        mAniPaint.setColor(selColor);
+
+        handleTypedArray(context, attrs);
     }
 
+    private void handleTypedArray(Context context, AttributeSet attrs) {
+        if (attrs == null) {
+            return;
+        }
+       /* TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.dot_indicator);
+        mIndicatorWidth = typedArray.getDimensionPixelSize(R.styleable.Banner_indicator_width, indicatorSize);
+        mIndicatorHeight = typedArray.getDimensionPixelSize(R.styleable.Banner_indicator_height, indicatorSize);
+        mIndicatorMargin = typedArray.getDimensionPixelSize(R.styleable.Banner_indicator_margin, BannerConfig.PADDING_SIZE);
+        mIndicatorSelectedResId = typedArray.getResourceId(R.styleable.Banner_indicator_drawable_selected, R.drawable.gray_radius);
+        mIndicatorUnselectedResId = typedArray.getResourceId(R.styleable.Banner_indicator_drawable_unselected, R.drawable.white_radius);
+        scaleType = typedArray.getInt(R.styleable.Banner_image_scale_type, scaleType);
+        delayTime = typedArray.getInt(R.styleable.Banner_delay_time, BannerConfig.TIME);
+        scrollTime = typedArray.getInt(R.styleable.Banner_scroll_time, BannerConfig.DURATION);
+        isAutoPlay = typedArray.getBoolean(R.styleable.Banner_is_auto_play, BannerConfig.IS_AUTO_PLAY);
+        titleBackground = typedArray.getColor(R.styleable.Banner_title_background, BannerConfig.TITLE_BACKGROUND);
+        titleHeight = typedArray.getDimensionPixelSize(R.styleable.Banner_title_height, BannerConfig.TITLE_HEIGHT);
+        titleTextColor = typedArray.getColor(R.styleable.Banner_title_textcolor, BannerConfig.TITLE_TEXT_COLOR);
+        titleTextSize = typedArray.getDimensionPixelSize(R.styleable.Banner_title_textsize, BannerConfig.TITLE_TEXT_SIZE);
+        mLayoutResId = typedArray.getResourceId(R.styleable.Banner_banner_layout, mLayoutResId);
+        bannerBackgroundImage = typedArray.getResourceId(R.styleable.Banner_banner_default_image, R.drawable.no_banner);
+        typedArray.recycle();*/
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -56,35 +94,86 @@ public class DotIndicator extends BaseIndicator {
 
     private void drawIndicatorsAnimation(Canvas canvas) {
         mAniPaint.setStyle(Paint.Style.FILL);
-        path2.reset();
-        path2.addRect(getRectIndicator(this.cPosition, this.cPositionOffset), Path.Direction.CW);
-        canvas.drawPath(path2, mAniPaint);
+        selIndicatorpath.reset();
+        selIndicatorpath.addRect(getSelIndicator(this.cPosition, this.cPositionOffset), Path.Direction.CW);
+        canvas.drawPath(selIndicatorpath, mAniPaint);
     }
 
     private void drawIndicators(Canvas canvas) {
 //        canvas.drawColor(Color.parseColor("#AA0000"));
         mPaint.setStyle(Paint.Style.FILL);
-        path.reset();
+        indicatorPath.reset();
         for (int i = 0; i < this.size; i++) {
-            path.addRect(getRectIndicator(i, 0), Path.Direction.CW);
+            indicatorPath.addRect(getDefIndicator(i, 0), Path.Direction.CW);
         }
-        canvas.drawPath(path, mPaint);
+        canvas.drawPath(indicatorPath, mPaint);
     }
 
-    private RectF getRectIndicator(int position, float positionOffset) {
-        int left = position * (indicatorWidth + indicatorMargin)+100;
-        if (position!=this.size-1) {
-            left += (int) ((indicatorWidth + indicatorMargin) * positionOffset);
+    /**
+     * 默认的指示器样式
+     *
+     * @param position
+     * @param positionOffset
+     * @return
+     */
+    private RectF getDefIndicator(int position, float positionOffset) {
+        int measuredWidth = getMeasuredWidth();
+        int measuredHeight = getMeasuredHeight();
+
+        int width = measuredWidth / this.size;
+
+
+        //根据绘制的宽度高度决定绘制的起点
+        //中心x
+        int centerX = position * width + (width - indicatorMargin) / 2;
+        int centerY = measuredHeight / 2;
+
+        Log.d("test", "总高度: " + measuredHeight + "总宽度: " + measuredWidth + "---中心点x" + centerX + "---中心点y" + centerY);
+
+        int left = centerX - indicatorWidth / 2;
+
+//        int left = position * (indicatorWidth + indicatorMargin);
+
+        int top = centerY - indicatorHeight / 2;
+
+        int right = left + indicatorWidth;
+        int bottom = top + indicatorHeight;
+        return new RectF(left, top, right, bottom);
+    }
+
+    /**
+     * 选中指示器样式
+     *
+     * @param position
+     * @param positionOffset
+     * @return
+     */
+    private RectF getSelIndicator(int position, float positionOffset) {
+        int measuredWidth = getMeasuredWidth();
+        int measuredHeight = getMeasuredHeight();
+
+        int width = (measuredWidth) / this.size;
+
+        //根据绘制的宽度高度决定绘制的起点
+        //中心x
+        int centerX = position * width + (width - indicatorMargin) / 2;
+        int centerY = measuredHeight / 2;
+
+        int left = centerX - selIndicatorWidth / 2;
+//        int left = position * (indicatorWidth + indicatorMargin);
+        int top = centerY - selIndicatorHeight / 2;
+
+        if (position != this.size - 1) {
+            left += (int) ((selIndicatorWidth + indicatorMargin) * positionOffset);
         }
-        int top = 0;
-        int right = left + 40;
-        int bottom = indicatorHeight;
+
+        int right = left + selIndicatorWidth;
+        int bottom = top + selIndicatorHeight;
         return new RectF(left, top, right, bottom);
     }
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        Log.d("test", "position: " + position + "positionOffset: " + positionOffset + "-positionOffsetPixels--" + positionOffsetPixels);
         cPosition = position;
         cPositionOffset = positionOffset;
         invalidate();
@@ -94,4 +183,21 @@ public class DotIndicator extends BaseIndicator {
     public void createIndicator(int size) {
         this.size = size;
     }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int selWidth = (selIndicatorWidth + indicatorMargin) * this.size;
+        int defWidth = (indicatorWidth + indicatorMargin) * this.size;
+        int width = selWidth > defWidth ? selWidth : defWidth;
+
+        int height = selIndicatorHeight > indicatorHeight ? selIndicatorHeight : indicatorHeight;
+
+        int measuredWidth = resolveSize(width, widthMeasureSpec);
+        int measuredHeight = resolveSize(height, heightMeasureSpec);
+
+        // 再用 setMeasuredDimension(measuredWidth, height) 来保存最终的宽度和高度
+        setMeasuredDimension(measuredWidth, measuredHeight);
+    }
+
+
 }
